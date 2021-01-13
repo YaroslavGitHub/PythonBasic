273459a1398531123dd6084cdc84d3c22f60995b
Yaroslav <4cast@ukr.net>
2021-01-13 14:17:34 +0200

    py random practice

--- index.py
+++ index.py
@@ -1,13 +1,14 @@
-import random, string
-
-smallcaps = 'abcdefghijklmnopqrstuvwxyz'
-largecaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-digits = '0123456789'
-letters_numbers = string.ascii_letters + string.digits
-word = ''
-for i in range(7):
-    word += random.choice(letters_numbers)
-word1 = ''.join(random.sample(letters_numbers,7))
-#word = random.choices(letter_numbers, k=7)    
-print(word)
-print(word1)
+#import modules
+from  random import randrange as r 
+# ask how many questions user wants
+no_questions = int(input('How many questions do you want?: '))
+#set score start at zero
+score = 0
+#loop through number of questions
+for q in range(no_questions):
+    num1,num2 = r(1,11),r(1,11)
+    ans = num1 * num2
+    u_ans =int(input(f'{num1} X {num2} = '))
+    if u_ans == ans:
+        score += 1
+print(f'Thank you for playing! \nYou got {score} out of {no_questions} {round(score/no_questions*100)}%)')
